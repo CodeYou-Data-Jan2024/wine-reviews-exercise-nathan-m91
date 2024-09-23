4e4d61eee621c39ac5d6ca20189564c61654a164
nathan-m91 <nathan.mason0731@gmail.com>
2024-09-22 21:06:47 -0400

    finished wine review

--- reviews.py
+++ reviews.py
@@ -1,2 +1,10 @@
-# add your code here
+import pandas as pd
 
+wine_reviews = pd.read_csv("C:/Users/moose/Documents/Projects/Wine Reviews/wine-reviews-exercise-nathan-m91/winemag-data-130k-v2.csv", index_col=0)
+data = wine_reviews[['country', 'points']]
+csv_data = data.groupby('country').aggregate(count=('points', 'size'), points=('points', 'mean'))
+csv_data['points'] = csv_data['points'].round(1)
+final = csv_data.sort_values(by='count', ascending=False).reset_index()
+final_df = pd.DataFrame(final)
+final_df.to_csv('reviews-per-country.csv')
+print(final_df)
